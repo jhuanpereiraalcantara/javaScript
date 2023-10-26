1a830aad7eb91f31f35090091b6ad29eda1e6bf8
shiba070 <113600837+jhuanpereiraalcantara@users.noreply.github.com>
2023-10-26 09:08:48 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,22 @@
+ function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+ }
+
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+ 
+ let contador = 0;
+ 
+ //enquanto
+ while (contador < listaDeTeclas.length) { 
+
+    listaDeTeclas[contador].onclick = function () {
+     tocaSom ('#som_tecla_pom');
+    }
+    
+    
+    contador = contador + 1;
+
+    console.log (contador);
+ }
+
+    
